817745dd973cc84eae42954b872eebaefc1bf70a
pedrocfranca <146142146+pedrocfranca@users.noreply.github.com>
2023-10-08 14:43:29 -0300

    Update snake.js
    adição de estrutura if else para adicionar mais maçãs no jogo

--- snake.js
+++ snake.js
@@ -12,15 +12,41 @@ const pointEq = p1 => p2 => p1.x == p2.x && p1.y == p2.y
 
 // Booleans
 const willEat   = state => pointEq(nextHead(state))(state.apple)
-const willEat2   = state => pointEq(nextHead(state))(state.mouse)
+const willEatMouse   = state => pointEq(nextHead(state))(state.mouse)
 const willCrash = state => state.snake.find(pointEq(nextHead(state)))
 const validMove = move => state =>
   state.moves[0].x + move.x != 0 || state.moves[0].y + move.y != 0
 
 // Next values based on state
 const nextMoves = state => state.moves.length > 1 ? dropFirst(state.moves) : state.moves
-const nextApple = state => willEat(state) ? rndPos(state) : state.apple
-const nextmouse = state => willEat2(state) ? rndPos(state) : state.mouse
+const nextApple = state => {
+  if (willEat(state)) {
+    return rndPos(state);
+  } else if (willEatMouse(state)){
+    return rndPos(state);
+  } else {
+    return state.apple;
+  }
+}
+const nextApple2 = state => {
+  if (willEat2(state)) {
+    return rndPos(state);
+  } else if (willEatMouse(state)){
+    return rndPos(state);
+  } else {
+    return state.apple2;
+  }
+}
+const nextApple3 = state => {
+  if (willEat3(state)) {
+    return rndPos(state);
+  } else if (willEatMouse(state)){
+    return rndPos(state);
+  } else {
+    return state.apple3;
+  }
+}
+const nextMouse = state => willEatMouse(state) ? rndPos(state) : state.mouse
 const nextHead  = state => state.snake.length == 0
   ? { x: 2, y: 2 }
   : {
@@ -55,7 +81,7 @@ const next = spec({
   moves: nextMoves,
   snake: nextSnake,
   apple: nextApple,
-  mouse: nextmouse
+  mouse: nextMouse
 })
 
 const enqueue = (state, move) => validMove(move)(state)
